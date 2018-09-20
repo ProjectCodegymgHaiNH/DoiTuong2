bc5f3a233773909f191f4d10c2640bceee87beeb
= <=>
2018-09-20 09:08:03 +0700

    bai tap pikachu hoan thien

--- objects/script.js
+++ objects/script.js
@@ -1,17 +1,13 @@
-/**
- * Created by nhatnk on 4/26/17.
- */
-
-function Hero(image, top, left, size) {
+let Hero = function(image, top, left, size) {
     this.image = image
     this.top = top || 0
     this.left = left || 0
     this.size = size || 100
 
-    this.stopR = true
-    this.stopB = false
-    this.stopL = false
-    this.stopT = false
+    this.stopR = false
+    this.stopB = true
+    this.stopL = true
+    this.stopT = true
 
     this.getHeroElement = function () {
         return '<img width="' + this.size + '"' +
@@ -29,52 +25,40 @@ function Hero(image, top, left, size) {
         this.top += so;
         console.log("bottom: " + this.top)
     }
-
-    this.moveLeft = function (so) {
-        this.left -= so;
-        console.log("right: " + this.right)
-    }
-
-    this.moveTop = function (so) {
-        this.top -= so;
-        console.log("top: " + this.bottom)
-    }
 }
 
-var hero = new Hero('doraemon.jpg', 0, 0, 200);
+let hero = new Hero('doraemon.jpg', 0, 0, 200);
 
-function start() {
-        if (hero.left < window.innerWidth - hero.size - 50 && hero.stopR && hero.stopT == false && hero.stopL == false && hero.stopB ==false) {
-            hero.moveRight(50)
-        } else {
-            hero.stopR = false
-            hero.stopB = true
-        }
+function start1() {
+    if (hero.left < window.innerWidth - hero.size - 50 && hero.stopR == false && hero.stopT && hero.stopL && hero.stopB) {
+        hero.moveRight(50)
+    } else {
+        hero.stopR = true
+        hero.stopB = false
+    }
 
+    if (hero.top < window.innerHeight - hero.size - 50 && hero.stopR && hero.stopT && hero.stopB == false) {
+        hero.moveBottom(50)
+    } else {
+        hero.stopB = true
+        hero.stopL = false
+    }
 
-        if (hero.top < window.innerHeight - hero.size -50 && hero.stopR== false && hero.stopT==false && hero.stopB) {
-            hero.moveBottom(50)
-        } else {
-            hero.stopB = false
-            hero.stopL = true
-        }
-
-        if (hero.left>0 && hero.stopR == false && hero.stopB == false && hero.stopL) {
-            hero.moveRight(-50);
-        } else {
-            hero.stopL = false
-            hero.stopT = true
-        }
+    if (hero.left > 0 && hero.stopR && hero.stopB && hero.stopL == false) {
+        hero.moveRight(-50);
+    } else {
+        hero.stopL = true
+        hero.stopT = false
+    }
     //
-        if (hero.top>0 && hero.stopB == false && hero.stopL == false && hero.stopT) {
-            hero.moveBottom(-50);
-        } else {
-            hero.stopT = false
-            hero.stopR = true
-        }
+    if (hero.top > 0 && hero.stopB && hero.stopL && hero.stopT == false) {
+        hero.moveBottom(-50);
+    } else {
+        hero.stopT = true
+        hero.stopR = false
+    }
     //
-    setTimeout(start, 100)
+    setTimeout(start1, 100)
     document.getElementById('game').innerHTML = hero.getHeroElement();
 }
-
-start()
+start1()
